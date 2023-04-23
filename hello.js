18bd2807a70c7e79ab5b6c31a5c4d21778676d53
tmoth89 <timsort101@gmail.com>
2023-04-23 12:33:03 -0400

    Made changes to app.

--- hello.js
+++ hello.js
@@ -1,11 +1,26 @@
-const express = require('express');
+
+const pulsar = require('pulsar-client');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello, DevOps Engineer!');
+const client = new pulsar.Client({
+    serviceUrl: 'pulsar://pulsar-proxy:6650',
+});
+
+async function createProducer() {
+    const producer = await client.createProducer({
+        topic: 'my-topic',
+    });
+    return producer;
+}
+
+app.get('/', async (req, res) => {
+    const producer = await createProducer();
+    producer.send({
+        data: 'Producer to the rescue!',
+    });
+    res.send('Hello, DevOps Engineer.');
 });
 
 app.listen(3000, () => {
-  console.log('App listening on port 3000!');
+    console.log('App listening on port 3000!');
 });
-
