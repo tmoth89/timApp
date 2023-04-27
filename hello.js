4190f86333680b09df8d3dec451b58abc5c12881
tmoth89 <timsort101@gmail.com>
2023-04-27 13:33:10 -0400

    Added file.

--- hello.js
+++ hello.js
@@ -7,21 +7,25 @@ const client = new pulsar.Client({
 });
 
 async function createProducer() {
-    
-        const producer = await client.createProducer({
-            topic: 'my-topic',
-        });
-        return producer;
+    const producer = await client.createProducer({
+        topic: 'my-topic',
+    });
+    return producer;
 }
 
 app.get('/', async (req, res) => {
-    const producer = await createProducer();
-    producer.send({
-        data: 'Producer to the Rescue!',
-    });
-    res.send('Hello, DevOps Engineer.');
+    try {
+        const producer = await createProducer();
+        await producer.send({
+            data: 'Producer to the Rescue!',
+        });
+        res.send('Hello, Devops Engineer!');
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Failed to send message.');
+    }
 });
 
 app.listen(3000, () => {
-    console.log('App listening on port 80');
+    console.log('App listening on port 3000');
 });
